ccfe2ad86bfc0d2cdd4268ad34a8ebb30a120a16
Dylan Ocampo <dylanomar@live.com.mx>
2024-09-19 15:29:28 -0600

    Login-Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
+import "./login.css"
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Home } from './pages/Home';
+import { LogIn } from './pages/LogIn';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="Appcontainer">
-      <Home></Home>
+      <Router>
+        <Routes>
+          <Route path= "" element={<LogIn></LogIn>}/>
+          <Route path= "home" element={<Home></Home>}/>
+        </Routes>
+        
+      </Router>
+
     </div>
   );
 }
