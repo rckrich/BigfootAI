a5784af7caa44fe8c5a322ea02c914f8ca8b5045
Dylan Ocampo <dylanomar@live.com.mx>
2024-10-30 11:03:47 -0600

    Build prueba

--- src/pages/AuthContext.js
+++ src/pages/AuthContext.js
@@ -11,10 +11,11 @@ export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
   useEffect(() => {
     if(userData=== null || userData===undefined) {
+      console.log("2");
       const fetchItems = async () => {
         const savedItems = await getItems();
         if (savedItems !== undefined) {
-          
+          console.log("3");
           setFlag(false);
           if(savedItems.length === 1) {
             setHasInfoBd(true);
@@ -56,10 +57,8 @@ export const AuthProvider = ({ children }) => {
   }, [getItems()]); // 
 
   useEffect(() => {
-    console.log(flag);
-    
+
     if(userData!== null && userData!==undefined){
-      console.log(userData);
       navigate("/home");
     }
     
@@ -75,17 +74,79 @@ export const AuthProvider = ({ children }) => {
   }
 
   const changeUserData = (newValue) => {
+
     let items = {}
     items.userData = newValue;
     items.ThreadName = "";
     items.ThreadId = "";
     items.userData.open_ia_key = "";
     saveItem(items)
-    setUserData(newValue);
+
+    fetch('http://165.22.178.7/back/api/v1/openaikey', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${newValue.access_token}`,
+      },
+      
+    })
+    .then(response => {
+      if (!response.ok) {
+      throw new Error(`Error del servidor`);
+    }
+    return response.json();
+    })
+      .then(data => {
+        newValue.open_ia_key = data.open_ia_key;
+        console.log(newValue);
+        setUserData(newValue);
+
+      })
+      .catch(error => {console.error('Error:', error)
+
+      });
+
+  };
+
+  const fetchItems = async () => {
+    const savedItems = await getItems();
+    if (savedItems !== undefined) {
+      console.log("3");
+      setFlag(false);
+      if(savedItems.length === 1) {
+        setHasInfoBd(true);
+        fetch('http://165.22.178.7/back/api/v1/openaikey', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${savedItems[0].userData.access_token}`,
+          },
+          
+        })
+        .then(response => {
+          if (!response.ok) {
+          throw new Error(`Error del servidor`);
+        }
+        return response.json();
+        })
+          .then(data => {
+            savedItems[0].userData.open_ia_key = data.open_ia_key;
+            setUserData(savedItems[0].userData);
+            
+          })
+          .catch(error => {console.error('Error:', error)
+            setHasInfoBd(false);
+          });
+
+      }
+      if(savedItems.length > 1){
+        CleanDb();
+      }
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ userData, changeUserData, hasInfoBD }}>
+    <AuthContext.Provider value={{ userData, changeUserData, hasInfoBD, setUserData }}>
       {children}
     </AuthContext.Provider>
   );
